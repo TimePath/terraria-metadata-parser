35449f8d8c12bb50f0d65823a8635db573418727
TimePath <andrew.hardaker1995@gmail.com>
2014-07-04 13:58:00 +1000

    Naming

--- map.py
+++ map.py
@@ -1,7 +1,7 @@
 # !/usr/bin/env python3
 import zlib
 import io
-
+from enum import Enum
 from struct import *
 
 
@@ -9,20 +9,20 @@ class BinaryStream:
     def __init__(self, base_stream):
         self.base_stream = base_stream
 
-    def read_byte(self):
-        return self.base_stream.read(1)
+    def read(self, limit=None):
+        return self.base_stream.read(limit)
 
-    def read_bytes(self, length):
-        return self.base_stream.read(length)
-
-    def read_char(self):
-        return self.unpack('b')
-
-    def read_uchar(self):
-        return self.unpack('B')
+    def unpack(self, fmt, length=1):
+        return unpack(fmt, self.read(length))[0]
 
     def read_bool(self):
         return self.unpack('?')
+
+    def read_int8(self):
+        return self.unpack('b')
+
+    def read_uint8(self):
+        return self.unpack('B')
 
     def read_int16(self):
         return self.unpack('h', 2)
@@ -37,20 +37,23 @@ class BinaryStream:
         return self.unpack('I', 4)
 
     def read_string(self):
-        length = self.read_uchar()
-        return self.unpack(str(length) + 's', length)
+        length = self.read_uint8()
+        return self.unpack(str(length) + 's', length).decode("utf-8")
 
-    def write_bytes(self, value):
+    def write(self, value):
         self.base_stream.write(value)
 
-    def write_char(self, value):
-        self.pack('c', value)
-
-    def write_uchar(self, value):
-        self.pack('C', value)
+    def pack(self, fmt, data):
+        return self.write(pack(fmt, data))
 
     def write_bool(self, value):
         self.pack('?', value)
+
+    def write_int8(self, value):
+        self.pack('c', value)
+
+    def write_uint8(self, value):
+        self.pack('C', value)
 
     def write_int16(self, value):
         self.pack('h', value)
@@ -66,35 +69,40 @@ class BinaryStream:
 
     def write_string(self, value):
         length = len(value)
-        self.write_uchar(length)
+        self.write_uint8(length)
         self.pack(str(length) + 's', value)
 
-    def pack(self, fmt, data):
-        return self.write_bytes(pack(fmt, data))
 
-    def unpack(self, fmt, length=1):
-        return unpack(fmt, self.read_bytes(length))[0]
+class Header(Enum):
+    Empty = 0
+    Tile = 1
+    Wall = 2
+    Water = 3
+    Lava = 4
+    Honey = 5
+    HeavenAndHell = 6
+    Background = 7
 
 
-def read_bits(count, read):
+def read_bits(count, read_byte):
     arr = []
-    b = 0
+    byte = 0
     bit = 128
-    for x in range(count):
+    for i in range(count):
         if bit == 128:
-            b = read()
+            byte = read_byte()
             bit = 1
         else:
             bit <<= 1
-        arr.append((b & bit) == bit)
+        arr.append((byte & bit) == bit)
     return arr
 
 
-def read_with_bits(bits, read):
+def read_with_bits(bitfield, read_byte):
     arr = []
-    for i in range(len(bits)):
-        if bits[i]:
-            arr.append(read())
+    for bit in bitfield:
+        if bit:
+            arr.append(read_byte())
         else:
             arr.append(1)
     return arr
@@ -103,70 +111,69 @@ def read_with_bits(bits, read):
 if __name__ == '__main__':
     with open('/home/andrew/My Games/Terraria/Players/TimePath.bak/1162047362.map', 'rb') as fd:
         stream = BinaryStream(fd)
-        curRelease = stream.read_int32()
-
-        print('Release: {0}'.format(curRelease))
+        version = stream.read_int32()
 
         worldName = stream.read_string()
         worldID = stream.read_int32()
         maxTilesY = stream.read_int32()
         maxTilesX = stream.read_int32()
 
-        TileIDCount = stream.read_int16()
-        WallIDCount = stream.read_int16()
-        maxLiquidTypes = stream.read_int16()
+        tileIDCount = stream.read_int16()
+        wallIDCount = stream.read_int16()
+        liquidTypes = stream.read_int16()
 
-        # maxSkyGradients, maxDirtGradients, maxRockGradients
-        num7 = stream.read_int16()
-        num8 = stream.read_int16()
-        num9 = stream.read_int16()
+        skyGradients = stream.read_int16()
+        dirtGradients = stream.read_int16()
+        rockGradients = stream.read_int16()
 
-        print(worldName)
-        print(maxTilesY)
-        print(maxTilesX)
+        print('''terraria-metadata-parser
+Format: {0}
+world: {1}
+width: {2}
+height: {3}'''.format(version, worldName, maxTilesX, maxTilesY))
 
-        array3bits = read_bits(TileIDCount, stream.read_char)
-        array4bits = read_bits(WallIDCount, stream.read_char)
-        array3 = read_with_bits(array3bits, stream.read_char)
-        print('Offset: {0}'.format(stream.base_stream.tell()))
-        array4 = read_with_bits(array4bits, stream.read_char)
+        maxTileOptionsBits = read_bits(tileIDCount, stream.read_int8)
+        maxWallOptionsBits = read_bits(wallIDCount, stream.read_int8)
 
-        num12 = int(sum(array3) + sum(array4) + maxLiquidTypes + num7 + num8 + num9 + 2)
-        print('Offset: {0}'.format(stream.base_stream.tell()))
-        compressed = stream.read_bytes(None)
-        print('Offset: {0}'.format(stream.base_stream.tell()))
-        print('Compressed size: {0}'.format(len(compressed)))
+        maxTileOptions = read_with_bits(maxTileOptionsBits, stream.read_int8)
+        maxWallOptions = read_with_bits(maxWallOptionsBits, stream.read_int8)
+
+        typeCount = 2 + sum(maxTileOptions) + sum(maxWallOptions) \
+                    + liquidTypes + skyGradients + dirtGradients + rockGradients
+        compressed = stream.read()
         bytestr = zlib.decompress(compressed, -zlib.MAX_WBITS)
         stream = BinaryStream(io.BytesIO(bytestr))
 
-        for l in range(maxTilesY):
-            for m in range(maxTilesX):
-                b3 = stream.read_char()
-                b4 = 0
-                if (b3 & 0b1) == 0b1: b4 = stream.read_char()
-                b5 = (b3 & 0b1110) >> 1
+        for y in range(maxTilesY):
+            for x in range(maxTilesX):
+                n1 = stream.read_int8()
+                n2 = 0
+                if (n1 & 0b1) == 0b1:
+                    n2 = stream.read_int8()
 
-                flag = {1: True, 2: True, 7: True}.get(b5, False)
-                num27 = 0
+                b5 = (n1 & 0b1110) >> 1
+
+                flag = {Header.Tile: True, Header.Wall: True, Header.Background: True}.get(b5, False)
+                typeIndex = 0
                 if flag:
-                    if (b3 & 0b1111) == 0b1111:
-                        num27 = stream.read_uint16()
+                    if (n1 & 0b1111) == 0b1111:
+                        typeIndex = stream.read_uint16()
                     else:
-                        num27 = stream.read_char()
+                        typeIndex = stream.read_int8()
 
-                light = 255  # light, 255 has special meaning
-                if (b3 & 0b100000) == 0b100000:
-                    light = stream.read_char()
+                light = 255  # 255 has special meaning
+                if (n1 & 0b100000) == 0b100000:
+                    light = stream.read_int8()
 
-                n = {1: stream.read_char, 2: stream.read_int16}.get((b3 & 0b11000000) >> 6, lambda: 0)()
+                remaining = {1: stream.read_int8, 2: stream.read_int16}.get((n1 & 0b11000000) >> 6, lambda: 0)()
 
                 if not b5:
-                    m += n
+                    x += remaining
                     continue
 
                 # Other map stuff
 
-                for i in range(n):
-                    m += 1
+                for i in range(remaining):
+                    x += 1
                     if light != 255:
-                        light2 = stream.read_char()
+                        light2 = stream.read_int8()
